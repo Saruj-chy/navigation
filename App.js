aa8c515cef6cfd7f5171db6d2bc43210a12bb553
Saruj-chy <saruj.chy222@gmail.com>
2020-08-06 00:36:09 +0600

    navigation drawer

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { StyleSheet, View, Text, Button } from 'react-native';
 
 import HomeScreen from './src/home';
@@ -8,37 +9,48 @@ import UserScreen from './src/users';
 import LogoTitle from './src/logo';
 
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen,
-    navigationOptions: ({ navigation }) => ({
-      title: `Home`,
-      // headerTitle: LogoTitle,
-      headerBackTitle: 'clown',
-    })
-  },
-  Users: UserScreen,
-},
-  {
-    initialRouteName: 'Home',
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: 'green'
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-        flex: 1,
-        textAlign: 'center'
+const AppNavigator = createDrawerNavigator({
+  Home: HomeScreen,
+  Users: UserScreen
+})
 
-      },
 
-    },
-    // headerMode: 'none',
-    // onTransitionStart: () => {
-    //   // alert('start')
-    // }
-  }
-)
+
+
+
+
+
+// const AppNavigator = createStackNavigator({
+//   Home: {
+//     screen: HomeScreen,
+//     navigationOptions: ({ navigation }) => ({
+//       title: `Home`,
+//       // headerTitle: LogoTitle,
+//       headerBackTitle: 'clown',
+//     })
+//   },
+//   Users: UserScreen,
+// },
+//   {
+//     initialRouteName: 'Home',
+//     defaultNavigationOptions: {
+//       headerStyle: {
+//         backgroundColor: 'green'
+//       },
+//       headerTintColor: '#fff',
+//       headerTitleStyle: {
+//         fontWeight: 'bold',
+//         flex: 1,
+//         textAlign: 'center'
+
+//       },
+
+//     },
+//     // headerMode: 'none',
+//     // onTransitionStart: () => {
+//     //   // alert('start')
+//     // }
+//   }
+// )
 
 export default createAppContainer(AppNavigator);
